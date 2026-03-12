2b9226914bf14480b51862f648cfcb8d1a0058f1
Sameer <sameerbashir522@gmail.com>
2026-03-12 16:16:32 +0530

    fix: resolve mime type error with relative paths and netlify.toml

--- arts/vite.config.js
+++ arts/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  base: './', // Add this line
+  base: './', 
   plugins: [react(), tailwindcss(), svgr()],
 })
